dfdfffaba34414ad5d6185fb8cdb3f28b31422be
warsaiqbal <160345806+warsaiqbal@users.noreply.github.com>
2024-06-05 13:33:41 -0700

    Add files via upload

---
+++ 34. Animals/app.ts
@@ -0,0 +1,7 @@
+let animals: string[] = ["dog", "horse", "parrot"];
+
+animals.forEach(animal => {
+    console.log(`A ${animal} would make a great pet.`);
+});
+console.log("Any of these animals would make great pets!");
+
